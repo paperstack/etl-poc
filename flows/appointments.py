584ece98d7a6f5aba047b532ae1892d15100215a
Lisa Liu <lisa.liu@stellar.health>
2021-01-08 11:55:08 -0500

    add option to read file from sftp, fail post_graph task when api errors

--- flows/appointments.py
+++ flows/appointments.py
@@ -12,7 +12,11 @@ validation_task = RunGreatExpectationsValidation()
 with Flow("St. Lukes Appointments ETL") as flow:
   input_file_path = Parameter("input_file_path", default="/Users/alex/development/projects/stellar/sfe/fixtures/tests/sample_data/stlukes_appointments/St_Lukes_Sample_Appointments_20201207.txt")
   ge_ctx_root = Parameter("ge_ctx_root", default="/Users/alex/development/projects/prefect_etl/great_expectations")
-  df = extract_data_frame(input_file_path)
+
+  sftp_password = Parameter('sftp_password')
+  sftp_file_path = Parameter('sftp_file_path')
+
+  df = extract_data_frame(input_file_path, sftp_password, sftp_file_path)
   validation_task(
     batch_kwargs={"dataset": df, "datasource": "appts"},
     expectation_suite_name="STLUKES_DEV_6K_APPTS.warning",
