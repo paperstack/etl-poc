998bb6025401a650f62440b2863dc79b0246abec
Alex Rud <alex.rud@stellar.health>
2021-01-08 20:07:58 -0500

    test setup

--- flows/appointments.py
+++ flows/appointments.py
@@ -13,8 +13,8 @@ log = utilities.logging.get_logger()
 validation_task = RunGreatExpectationsValidation()
 
 with Flow("St. Lukes Appointments ETL") as flow:
-  input_file_path = Parameter("input_file_path", default="/Users/alex/development/projects/stellar/sfe/fixtures/tests/sample_data/stlukes_appointments/St_Lukes_Sample_Appointments_20201207.txt")
-  ge_ctx_root = Parameter("ge_ctx_root", default="/Users/alex/development/projects/prefect_etl/great_expectations")
+  input_file_path = Parameter("input_file_path", default="/app/St_Lukes_Sample_Appointments_20201207.txt")
+  ge_ctx_root = Parameter("ge_ctx_root", default="/app/great_expectations")
 
   sftp_password = Parameter('sftp_password')
   sftp_file_path = Parameter('sftp_file_path')
